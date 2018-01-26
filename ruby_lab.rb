2015620ee4207d4770421ae34d53abee516b5a7d
keefer <keefer212@gmail.com>
2018-01-26 08:00:59 -0700

    Added the cleanup method to grab titles

--- ruby_lab.rb
+++ ruby_lab.rb
@@ -12,21 +12,27 @@
 $bigrams = Hash.new # The Bigram data structure
 $name = "Keefer Sands"
 
-
+def cleanup_titles(title)
+		temp = title.split('<SEP>')
+		title = temp[2]
+		p title
+		return title
+end
 # function to process each line of a file and extract the song titles
 def process_file(file_name)
 	puts "Processing File.... "
-	lines = {}
-	begin
+	lines = []
+	#begin
 		IO.foreach(file_name) do |line|
+			line = cleanup_titles(line)
 			lines << line
 			# do something for each line
 		end
 		puts "Finished. Bigram model built.\n"
-	rescue
-		STDERR.puts "Could not open file"
-		exit 4
-	end
+	#rescue
+		#STDERR.puts "Could not open file"
+		#exit 4
+	#end
 end
 
 # Executes the program
